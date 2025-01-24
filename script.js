5cffc7ee7ef05a4f6a307de11195a3198ceec26a
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-24 20:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    // Create a paragraph element
+    const message = document.createElement("p");
+    message.textContent = "DOM load success"; // Set the message text
+    
+    // Append the paragraph to the body
+    document.body.appendChild(message);
+});
